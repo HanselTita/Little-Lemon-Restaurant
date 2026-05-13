01fe461e1453f2769e4543474bb67b3787e6c3db
Hansel <hanseltita@gmail.com>
2026-05-14 00:06:51 +0700

    feat: styling hero section in Main.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,44 @@
 import React from 'react'
+import HeroImage from '../assets/pic1.jpg'
+import './Main.css'
 
 function Main() {
   return (
-    <main>
-        <section>
+<main>
+
+<section className="hero">
+
+  <div className="hero-content">
+    <h1>Little Lemon</h1>
+
+    <h2>Chicago</h2>
+
+    <p>
+    We are a family owned African restaurant,
+    focused on traditional recipes served with a modern twist
+    </p>
+
+    <button>Reserve a Table</button>
+  </div>
+
+  <div className="hero-image">
+    <img src={HeroImage} alt="Little Lemon Food" />
+  </div>
 
 </section>
-<section>
-  
-</section>
-<section>
-  
-</section>
-<section>
-  
-</section>
-    </main>
+
+  <section className="specials">
+
+  </section>
+
+  <section className="testimonials">
+
+  </section>
+
+  <section className="about">
+  </section>
+
+</main>
   )
 }
 
