6f16b5cb3fbb47a7298b38b78bc94eed74a3dcc0
Hansel <hanseltita@gmail.com>
2026-05-14 21:34:03 +0700

    feat: styling special sections in Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,10 @@ import React from 'react'
 import HeroImage from '../assets/pic1.jpg'
 import './Main.css'
 
+import Rice from "../assets/jellof-rice.jpg";
+import Eru from "../assets/fufu-eru.jpg";
+import Fish from "../assets/katti-katti.jpg";
+
 function Main() {
   return (
 <main>
@@ -19,23 +23,91 @@ function Main() {
     </p>
 
     <button>Reserve a Table</button>
-  </div>
+    </div>
 
-  <div className="hero-image">
+    <div className="hero-image">
     <img src={HeroImage} alt="Little Lemon Food" />
+    </div>
+
+     </section>
+
+ <section className="specials">
+
+   <div className="specials-top">
+
+    <h2>This Weeks Specials!</h2>
+ 
+  <button>Online Menu</button>
+
+</div>
+
+<div className="specials-cards">
+
+<   div className="special-card">
+
+     <img src={Rice} alt="Jellof Rice" />
+
+    <div className="card-content">
+
+    <div className="card-top">
+      <h3>Jellof Rice</h3>
+      <span>$250</span>
+    </div>
+
+    <p>
+      Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit.
+    </p>
+
+    <h4>Order & Delivery</h4>
+
   </div>
 
-</section>
+</div>
 
-  <section className="specials">
+<div className="special-card">
 
-  </section>
+  <img src={Eru} alt="Fufu Eru" />
 
-  <section className="testimonials">
+  <div className="card-content">
 
-  </section>
+    <div className="card-top">
+      <h3>Fufu Eru</h3>
+      <span>$300</span>
+    </div>
 
-  <section className="about">
+    <p>
+      Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit.
+    </p>
+
+    <h4>Order & Delivery</h4>
+
+  </div>
+
+</div>
+
+<div className="special-card">
+
+  <img src={Fish} alt="Katti Katti" />
+
+  <div className="card-content">
+
+    <div className="card-top">
+      <h3>Katti katti</h3>
+      <span>$350</span>
+    </div>
+    <p>
+      Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit.
+    </p>
+    <h4>Order & Delivery</h4>
+  </div>
+
+</div>
+
+</div>
+
   </section>
 
 </main>
